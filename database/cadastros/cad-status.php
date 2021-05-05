c0c9a5744ea4faab59f2fab125346fe6099addb6
Samuel-Jesus <samuel.jesan2018@gmail.com>
2021-05-05 11:40:42 -0300

    Criado arquivo de cadastro de Status

---
+++ database/cadastros/cad-status.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+include_once '../conect.php';
+
+$cad_status = filter_input(INPUT_POST, 'cad_status', FILTER_SANITIZE_STRING);
+
+if(!empty($cad_status)){
+    //Guardando dados do formulário nas variáveis.
+    $status = filter_input(INPUT_POST, 'status', FILTER_SANITIZE_STRING);
+
+    $sql = "INSERT INTO status (nome) VALUES ( :nome )";
+
+    $insert_status = $conect->prepare($sql); 
+    $insert_status->bindValue(':nome', $status);
+        if (!empty($insert_status->execute())) {
+            $_SESSION['msg_status'] = '<div class="alert alert-success mT-10" role="alert"> Status cadastrado com sucesso! </div>';
+            header("location: ../../src/config.php");
+        } else {
+            $_SESSION['msg_status'] = '<div class="alert alert-danger mT-10" role="alert"> Erro ao cadastrar este Status, verifique os campos ou contate o suporte </div>';
+            header("location: ../../src/config.php");
+        }
+
+}else{
+    $_SESSION['msg_status'] = '<div class="alert alert-danger mT-10" role="alert"> Erro ao cadastrar este Status, verifique os campos ou contate o suporte </div>';
+    header("location: ../../src/config.php");
+}
