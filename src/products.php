94855d274b671dbc4eaf3f3eab14166b1c96857f
Samuel-Jesus <samuel.jesan2018@gmail.com>
2021-05-05 11:20:29 -0300

    Criando modais para cadastro de Usuário e Produtos(Finalizado), colocando Query para os Selects do cadastro.

--- src/products.php
+++ src/products.php
@@ -1,8 +1,31 @@
 <?php
+session_start();
+require_once '../database/conect.php';
 
+// Selecionado dados da tabela Status para o <option> </option>.
+    $sql_status = "SELECT * FROM status";
+    $show_table = $conect->prepare($sql_status);
+    $show_table->execute();
+    $dados_status = $show_table->fetchAll(PDO::FETCH_ASSOC);
+
+    $sql_categ = "SELECT * FROM categorias";
+    $show_table = $conect->prepare($sql_categ);
+    $show_table->execute();
+    $dados_categ = $show_table->fetchAll(PDO::FETCH_ASSOC);
+
+    $sql_marcas = "SELECT * FROM marcas";
+    $show_table = $conect->prepare($sql_marcas);
+    $show_table->execute();
+    $dados_marcas = $show_table->fetchAll(PDO::FETCH_ASSOC);
+
+    $sql_fornec = "SELECT * FROM fornecedores";
+    $show_table = $conect->prepare($sql_fornec);
+    $show_table->execute();
+    $dados_fornec = $show_table->fetchAll(PDO::FETCH_ASSOC);               
 ?>
+
 <!DOCTYPE html>
-<html>
+<html lang="pt-BR">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
@@ -115,7 +138,27 @@
               <span class="title"><div class="peer "><button type="button" class="btn cur-p btn-outline-dark" ><i class="c-blank-500 ti-user"></i> Adicionar Usuário</button></div></span>
             </a>
           </li>
-          
+          <li class="nav-item dropdown">
+            <a class="dropdown-toggle" href="javascript:void(0);"><span class="icon-holder"><i class="c-blank-500 ti-settings"></i></span>
+              <span class="title">Configurar</span> <span class="arrow"><i class="ti-angle-right"></i></span></a>
+                <ul class="dropdown-menu">
+                  <li class="nav-item dropdown"><a href="javascript:void(0);"><span class="icon-holder"><i class="c-blank-500 ti-plus"> </i></span>
+                    <span>Histórico de Notificações</span></a></li>
+                  <li class="nav-item dropdown"><a href="javascript:void(0);"><span class="icon-holder"><i class="c-blank-500 ti-user"> </i></span>
+                    <span>Usuários</span></a></li>
+                  <li class="nav-item dropdown">
+                    <a href="javascript:void(0);"><span class="icon-holder"><i class="c-blank-500 ti-plus"> </i></span>
+                    <span> Cadastros</span><span class="arrow"><i class="ti-angle-right"></i></span>
+                    </a>
+                    <ul class="dropdown-menu">
+                      <li><a href="config.php">Fornecedores</a></li>
+                      <li><a href="config.php">Marcas</a></li>
+                      <li><a href="config.php">Categorias</a></li>
+                      <li><a href="config.php#table_status">Status</a></li>
+                    </ul>
+                  </li>
+                </ul>
+          </li>
         </ul>
         <!-- FINAL MENU DE PAGINAS -->
       </div>
@@ -207,6 +250,12 @@
               <div class="col-md-12">
                 <div class="bgc-white bd bdrs-3 p-20 mB-20">
                   <h4 class="c-grey-900 mB-20">Produtos</h4>
+                  <?php 
+                    if (isset($_SESSION['msg_produtos'])){
+                        echo $_SESSION['msg_produtos'];
+                        unset($_SESSION['msg_produtos']);
+                    }
+                   ?>
                   <table id="dataTable" class="table table-striped table-bordered" cellspacing="0" width="100%">
                     <thead>
                       <tr>
@@ -276,6 +325,7 @@
                       
                     </tbody>
                   </table>
+                  
                 </div>
               </div>
             </div>
@@ -288,78 +338,6 @@
       </footer>
 
       <!-- MODAIS DA PÁGINA -->
-          <!-- Modal cadastro de produto -->
-          <div class="modal fade" id="cadproduto" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-lg" role="document">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLabel">Cadastrar Produto</h5>
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                </div>
-              <form method="POST"></form>
-                <div class="modal-body">
-                  
-                    <div class="form-row">
-                      <div class="form-group col-md-3"><label for="produtoCode">Código</label> 
-                        <input type="text" class="form-control" id="produtoCode" placeholder="Código do produto" name="code" >
-                      </div>
-                      <div class="form-group col-md-5"><label for="produtoName">Nome</label> 
-                        <input type="text" class="form-control" id="produtoName" placeholder="Nome do produto" name="name">
-                      </div>
-                      <div class="form-group col-md-3">
-                        <label for="produtoCategoria">Categoria</label> 
-                        <select  id="produtoCategoria" class="form-control" name="categoria">
-                          <option value="0">Sem Categoria</option>
-                        </select>
-                      </div>
-                    </div>
-                    <div class="form-group"><label for="produtoDescricao">Descrição</label> 
-                      <input type="text" class="form-control" id="produtoDescricao" placeholder="Descreva aqui o seu produto" name="desc">
-                    </div>
-                    <div class="form-row">
-                      <div class="form-group col-md-3">
-                        <label for="produtoFornecedor">Fornecedor</label> 
-                        <select id="produtoFornecedor" class="form-control" name="fornecedor">
-                          <option value="0">Sem Fornecedor</option>
-                        </select>
-                      </div>
-                      <div class="form-group col-md-3">
-                        <label for="inputMarca">Marca</label> 
-                        <select id="produtoMarca" class="form-control" name="marca">
-                          <option>Sem marca</option>
-                        </select>
-                      </div>
-                      <div class="form-group col-md-2"><label for="produtoQuantidade">Quantidade</label> 
-                        <input type="number" class="form-control" id="produtoQuantidade" placeholder="Quantidade" name="quantidade">
-                      </div>
-                      <div class="form-group col-md-2"><label for="produtoValor">Valor</label> 
-                        <input type="text" class="form-control" id="produtoValor" placeholder="Valor unitário" name="valor">
-                      </div>
-                      <div class="form-group col-md-2"><label for="produtoValor">Cor</label> 
-                        <input type="text" class="form-control" id="produtoValor" placeholder="Cor do produto" name="cor">
-                      </div>
-                    </div>
-                    <hr>
-                    <div class="form-row">
-                      <div class="form-group col-md-6">
-                        <label for="produtoFile">Arquivo Foto </label> 
-                        <input type="file" class="form-control" id="produtoFile" placeholder="JPEG ou PNG" name="foto">
-                      </div>
-                      <div class="col-md-4">
-                        <img src="#" alt="Imagem do produto">
-                      </div>
-                    </div>
-                  
-                </div>
-                <div class="modal-footer">
-                  <button type="reset" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                  <button type="submit" class="btn btn-primary">Cadatrar</button>
-                </div>
-              </form>
-              </div>
-            </div>
-          </div>
-          <!-- Fim Modal PRODUTOS-->
            <!-- Modal cadastro de usuários -->
            <div class="modal fade" id="caduser" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-lg" role="document">
@@ -423,7 +401,7 @@
                 </div>
                 <div class="modal-footer">
                   <button type="reset" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                  <button name="cad-product" type="submit" class="btn btn-primary">Cadatrar</button>
+                  <button type="submit" class="btn btn-primary">Cadatrar</button>
                 </div>
               </form>
 
@@ -431,6 +409,110 @@
             </div>
           </div>
           <!-- Fim Modal USUÁRIOS-->
+          <!-- Modal cadastro de produto -->
+          <div class="modal fade" id="cadproduto" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Cadastrar Produto</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                  <!-- Exibe a mensagem do resultado da inserção de dados -->                   
+                </div>
+              <form method="POST" action="../database/cadastros/cad_product.php">
+                <div class="modal-body">
+                    <div class="form-row">
+                      <div class="form-group col-md-2"><label for="produtoCode">Código</label> 
+                        <input type="text" class="form-control" id="produtoCode" placeholder="Código do produto" name="code" >
+                      </div>
+                      <div class="form-group col-md-4"><label for="produtoName">Nome</label> 
+                        <input type="text" class="form-control" id="produtoName" placeholder="Nome do produto" name="name">
+                      </div>
+                      <div class="form-group col-md-3">
+                        <label for="produtoCategoria">Categoria</label> 
+                        <select  id="produtoCategoria" class="form-control" name="categoria">
+                          <option value="0">Sem Categoria</option>
+                          <?php
+                          foreach ($dados_categ as $index => $dado){
+                            echo '<option value="'.$dado['id'].'">'.$dado['name']. '</option>';
+                          }
+                          ?> 
+                        </select>
+                      </div>
+                      <div class="form-group col-md-3">
+                        <label for="produtoCategoria">Status</label> 
+                        <select  id="produtoCategoria" class="form-control" name="status">
+                          <option value="0">Sem Categoria</option>
+                          <?php
+                          foreach ($dados_status as $index => $dado){
+                            echo '<option value="'.$dado['id'].'">'.$dado['nome']. '</option>';
+                          }
+                          ?> 
+                        </select>
+                      </div>
+                    </div>
+                    <div class="form-group"><label for="produtoDescricao">Descrição</label> 
+                      <input type="text" class="form-control" id="produtoDescricao" placeholder="Descreva aqui o seu produto" name="desc">
+                    </div>
+                    <div class="form-row">
+                      <div class="form-group col-md-3">
+                        <label for="produtoFornecedor">Fornecedor</label> 
+                        <select id="produtoFornecedor" class="form-control" name="fornecedor">
+                          <option value="0">Sem Fornecedor</option>
+                          <?php
+                          foreach ($dados_fornec as $index => $dado){
+                            echo '<option value="'.$dado['id'].'">'.$dado['nome']. '</option>';
+                          }
+                          ?> 
+                        </select>
+                      </div>
+                      
+                       
+                      <div class="form-group col-md-3">
+                        <label for="inputMarca">Marca</label> 
+                        
+                        <select id="produtoMarca" class="form-control" name="marca">
+                          <option>Sem marca</option>
+                         <?php
+                          foreach ($dados_marcas as $index => $dado){
+                            echo '<option value="'.$dado['id'].'">'.$dado['nome']. '</option>';
+                          }
+                          ?> 
+                        </select>
+                        
+                      </div>
+                      
+                      <div class="form-group col-md-2"><label for="produtoQuantidade">Quantidade</label> 
+                        <input type="number" class="form-control" id="produtoQuantidade" placeholder="Quantidade" name="quantidade">
+                      </div>
+                      <div class="form-group col-md-2"><label for="produtoValor">Valor</label> 
+                        <input type="text" class="form-control" id="produtoValor" placeholder="Valor unitário" name="valor">
+                      </div>
+                      <div class="form-group col-md-2"><label for="produtoCor">Cor</label> 
+                        <input type="text" class="form-control" id="produtoCor" placeholder="Cor do produto" name="cor">
+                      </div>
+                    </div>
+                    <hr>
+                    <div class="form-row">
+                      <div class="form-group col-md-6">
+                        <label for="produtoFile">Arquivo Foto </label> 
+                        <input type="file" class="form-control" id="produtoFile" placeholder="JPEG ou PNG" name="foto">
+                      </div>
+                      <div class="col-md-4">
+                        <img src="#" alt="Imagem do produto">
+                      </div>
+                    </div>
+                  
+                </div>
+                <div class="modal-footer">
+                  <button type="reset" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                  <button name="cad_product" type="submit" class="btn btn-primary" >Cadatrar</button>
+                </div>
+              </form>
+              </div>
+            </div>
+          </div>
+          <!-- Fim Modal PRODUTOS-->
+          
         
     </div>
   </div>
