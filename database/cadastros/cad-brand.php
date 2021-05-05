c40420fccf5094614b2f4558de892512c2c73ed4
Samuel-Jesus <samuel.jesan2018@gmail.com>
2021-05-05 11:42:34 -0300

    Criado arquivo de cadastro de Marcas

---
+++ database/cadastros/cad-brand.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+include_once '../conect.php';
+
+$cad_marca = filter_input(INPUT_POST, 'cad_marca', FILTER_SANITIZE_STRING);
+if (!empty($cad_marca)) {
+    //Guardando dados do formulário nas variáveis.
+    $marca        = filter_input(INPUT_POST, 'marca', FILTER_SANITIZE_STRING);
+
+    $sql = "INSERT INTO marcas (nome) VALUES ( :marca )";
+
+    $insert_marca = $conect->prepare($sql);
+    $insert_marca->bindValue(':marca', $marca);
+
+        if (!empty($insert_marca->execute())) {
+            $_SESSION['msg_marca'] = '<div class="alert alert-success mT-10" role="alert"> Marca cadastrada com sucesso! </div>';
+            header("location: ../../src/config.php");
+        } else {
+            $_SESSION['msg_marca'] = '<div class="alert alert-danger mT-10" role="alert"> Erro ao cadastrar esta Marca, verifique os campos ou contate o <b>suporte</b> </div>';
+            header("location: ../../src/config.php");
+        }
+} else {
+    $_SESSION['msg_marca'] = '<div class="alert alert-danger mT-10" role="alert"> Erro ao cadastrar este Marca, verifique os campos ou contate o <b>suporte</b> </div>';
+    header("location: ../../src/config.php");
+}
+
