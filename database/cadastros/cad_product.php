1c8e115c8fa5c950c6ce73e3dd25777a1307e2a2
Samuel-Jesus <samuel.jesan2018@gmail.com>
2021-05-05 23:22:09 -0300

    Adicionando inserção do caminho para a foto do formulario

--- database/cadastros/cad_product.php
+++ database/cadastros/cad_product.php
@@ -15,11 +15,13 @@ if(!isset($cad_product)){
     $categoria   = filter_input(INPUT_POST, 'categoria', FILTER_SANITIZE_NUMBER_INT);
     $status      = filter_input(INPUT_POST, 'status', FILTER_SANITIZE_NUMBER_INT);
     $fornecedor  = filter_input(INPUT_POST, 'fornecedor', FILTER_SANITIZE_NUMBER_INT);
+    $foto        = $_FILES['foto']['name'];
     
 
-    $sql = "CALL cad_products(:code, :nome, :valor, :qtt, :descri, :cor, :marca, :categoria, :fornecedor, :status )";
+    $sql = "CALL cad_products(:foto, :code, :nome, :valor, :qtt, :descri, :cor, :marca, :categoria, :fornecedor, :status )";
 
     $insert_produtos = $conect->prepare($sql);
+    $insert_produtos->bindValue(':foto', $foto);
     $insert_produtos->bindValue(':code', $code);
     $insert_produtos->bindValue(':nome', $name);
     $insert_produtos->bindValue(':valor', $valor);
@@ -32,10 +34,10 @@ if(!isset($cad_product)){
     $insert_produtos->bindValue(':fornecedor', $fornecedor);
         
     if (!empty($insert_produtos->execute())) {
-        $_SESSION['msg_produtos'] = '<div class="alert alert-success mT-10" role="alert"> Produto cadastrado com sucesso! </div>';
+        $_SESSION['msg_produtos'] = '<div class="alert alert-success mT-10" role="alert"> <b>Produto</b> cadastrado com sucesso! </div>';
         header("location: ../../src/products.php");
     } else {
-        $_SESSION['msg_produtos'] = '<div class="alert alert-danger mT-10" role="alert"> Erro ao cadastrar este Produto, verifique os campos ou contate o <b>suporte</b> </div>';
+        $_SESSION['msg_produtos'] = '<div class="alert alert-danger mT-10" role="alert"> Erro ao cadastrar este <b>Produto</b>, verifique os campos ou contate o <b>suporte</b> </div>';
         header("location: ../../src/products.php");
     }    
 
