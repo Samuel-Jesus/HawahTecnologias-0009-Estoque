b2a4893dee4a1786cad09e0463e8505cb022ea61
Samuel-Jesus <samuel.jesan2018@gmail.com>
2021-10-20 06:46:04 -0300

    Remodelando para OO usando class para conexao

--- database/conect.php
+++ database/conect.php
@@ -1,23 +1,30 @@
 <?php
-    $dbname = "db_nstore0001001";
-    $servername = "localhost";
-    $user = "root";
-    $pass = "";
+//Classe para conexão do BD (melhora na segurança dos dados pois usar *private)
+  class Connection{
+    private  $dbname = "db_nstore0001001"; // O private é um ***** que impossibilita que a variável seja usada fora da classe onde foi criada.
+    private  $servername = "127.0.0.1"; //''
+    private  $user = "root"; //''
+    private  $pass = ""; //''
+    private  $port = "3306";
+
+    public function conecting(){
+        //Vai testar se conexão está ocorrendo bem.
+      try {
+          //Guardando os dados na variável 'connected'.{$this->servername}
+          $connected = new PDO("mysql:host={$this->servername};port={$this->port};dbname={$this->dbname}", $this->user, $this->pass );
+          //Iniciando o teste de erro da conexão com o BD.
+          $connected->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //  echo "Você está conectado ao BD!";
+
+      }
+      //Se a conexão não for bem sucedida cai aqui e fecha a conexão.
+      catch(PDOException $e){
+        $connected = null;
+          //echo "Erro na conexão! Erro: " .$e->getMessage();
+      }
+
+      return $connected;
+    }
+  }
    
-    //Vai testar se conexão está ocorrendo bem.
-   try {
-       //Guardando os dados na variável 'conect'.
-       $conect = new PDO("mysql:host=$servername; dbname=$dbname", $user, $pass );
-
-       //Iniciando o teste de erro da conexão com o BD.
-       $conect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-     //  echo "Você está conectado ao BD!";
-   }
-   //Se a conexão não for bem sucedida cai aqui e mostra o erro.
-   catch(PDOException $e) {
-       echo "Erro na conexão! Erro: " .$e->getMessage();
-   }
-
-  
-   // $conn = null; fecha a conexão com o BD
 ?>
